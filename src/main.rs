921a400077d100b6fb832d16cfce6e5f1e1b96da
PlasticRosewood <jdtomjones@gmail.com>
2026-01-06 20:48:11 -0500

    Big upgrades to json parsing and add hashmap for easy lookup and register of target tags

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::{fs::File, io::BufReader, io::BufWriter, io::Write as IoWrite};
 use quick_xml::reader::Reader;
 use quick_xml::events::Event;
+use phf::phf_set;
 
 // The program will take slices of this large space array
 //      and use them to indent the json. Much faster than a loop
@@ -19,6 +20,18 @@ macro_rules! indent {
     };
 }
 
+static TARGET_TAGS: phf::Set<&'static [u8]> = phf_set! {
+    // Container Tags <tag></tag>
+    b"AudioTrack",
+    b"MidiTrack",
+    b"Branches",
+    b"DrumBranch",
+    
+    // Self closing tags <tag />
+    b"EffectiveName",
+    b"UserName",
+};
+
 fn main() -> std::io::Result<()> {
     let fin = File::open("Tutorial")?;
     let mut reader = Reader::from_reader(BufReader::new(fin));
@@ -28,52 +41,59 @@ fn main() -> std::io::Result<()> {
 
     let mut buf = Vec::new();
 
-    let mut level = 0; // current depth of the xml
-    let mut is_first = false;
+    let mut depth = 0;
+    let mut is_first = true;
 
     writer.write_all(b"{\n")?;
     loop {
         match reader.read_event_into(&mut buf) {
             // Start tag: Parse and increment level by one
             Ok(Event::Start(e) ) => {
-                match e.name().as_ref() {
-                    b"AudioTrack" | b"MidiTrack" => {
-                        // Format accounting for first item
-                        if !is_first { writer.write_all(b",\n")?; }
-                        else { is_first = false; }
-                        
-                        // Write tag found
-                        indent!(writer, level);
-                        let track_type = if e.name().as_ref() == b"AudioTrack" {"Audio Track"} else {"Midi Track"};
-                        writer.write_all(b"Found ")?;
-                        writer.write_all(track_type.as_bytes())?;
-                    }
-                    _ => (),
+                if TARGET_TAGS.contains(e.name().as_ref()) {
+                    // Format accounting for first item
+                    if !is_first { writer.write_all(b",\n")?; }
+                    
+                    // Write tag found
+                    indent!(writer, depth);
+                    writer.write_all(b"\"")?;
+                    writer.write_all(e.name().as_ref())?;
+                    writer.write_all(b"\" : {")?;
+                    is_first = true; // reset for internal indentation
                 }
-                level += 1;
+                depth += 1;
             }
 
             // Self closing tag: Parse and keep level the same
             Ok(Event::Empty(e)) => {
-                match e.name().as_ref() {
-                    b"EffectiveName" => {
-                        // Format accounting for first item
-                        if !is_first { writer.write_all(b",\n")?; }
-                        else { is_first = false; }
-
-                        indent!(writer, level);
-                        if let Ok(Some(attr)) = e.try_get_attribute("Value") {
-                            writer.write_all(b"Effective Name: ")?;
-                            writer.write_all(&attr.value)?;
-                        }
+                // Name of self closing tag
+                let name = e.name();
+                if TARGET_TAGS.contains(name.as_ref()) {
+                    if !is_first { writer.write_all(b",\n")?; }
+                    else { 
+                        writer.write_all(b"\n")?;
+                        is_first = false;
                     }
-                    _ => (),
+
+                    indent!(writer, depth);
+                    if let Ok(Some(attr)) = e.try_get_attribute("Value") {
+                        writer.write_all(b"\"")?;
+                        writer.write_all(name.as_ref())?;
+                        writer.write_all(b"\": \"")?;
+                        writer.write_all(&attr.value)?;
+                        writer.write_all(b"\"")?;
+                    }
                 }
             }
 
-            // Closing tag: Don't parse and decrement level by one
-            Ok(Event::End(_e)) => {
-                level -= 1;
+            // Closing tag
+            Ok(Event::End(e)) => {
+                depth -= 1;
+                if TARGET_TAGS.contains(e.name().as_ref()) {
+                    writer.write_all(b",\n")?;
+                    
+                    indent!(writer, depth);
+                    writer.write_all(b"}")?;
+                }
             }
             Ok(Event::Eof) => break,
             Err(e) => panic!("Error at position {}: {:?}", reader.buffer_position(), e),
